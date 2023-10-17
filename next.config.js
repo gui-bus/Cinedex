b08281c78f070dcc7468ad3e594c52850da40cad
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-17 14:44:01 -0300

    change: change homepage redirect

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/discover/now_playing",
+        destination: "/discover/tv",
         permanent: true,
       },
     ];
